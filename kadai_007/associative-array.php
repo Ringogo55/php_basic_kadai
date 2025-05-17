fb9f15dfa639dcb85e1c51533f1b7fb8f8dc9aab
Ringogo55 <akiko.yeah@gmail.com>
2025-05-17 10:23:22 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html ja>
+
+<head>
+  <p>
+    <?php
+    //連想配列に値を代入する
+    $item_data = ['name' =>'onion', 'price' => '200', 'weight'=>'160'];
+
+    //連想配列の値を出力する
+    print_r($item_data);
+
+    ?>
+
+</p>
+  
+</body>
+
+</html>
