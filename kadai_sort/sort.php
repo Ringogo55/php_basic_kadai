ab8cfd33ed2321b2fc96fd7c550848dd756fad2a
Ringogo55 <akiko.yeah@gmail.com>
2025-05-22 22:51:35 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<ltml lang="ja">
+
+<head>
+  <meta charset="UTF-8">  
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  
+ 
+  <p>
+    <?php
+      function sort_2way($nums, $order) {
+        if($order){
+          echo '昇順にソートします。<br>';
+          asort($nums);
+          foreach ($nums as $num) {
+            echo $num . '<br>';
+          }
+        } else {
+          echo '降順にソートします<br>';
+          arsort($nums);
+          foreach ($nums as $num) {
+          echo $num . '<br>';
+        }
+      }
+      }  
+
+      $nums = [15, 4, 18, 23, 10 ];
+      sort_2way($nums, true);
+      sort_2way($nums, false);
+
+?>
+</P>
+  </body>
+
+</html>
